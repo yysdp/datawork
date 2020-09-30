73420431d98e0a60873bd4447a10db340372ad49
张海亮 <50296108+yysdp@users.noreply.github.com>
2020-09-30 17:40:00 +0800

    保存数据完成

--- 数据挖掘哈希树.py
+++ 数据挖掘哈希树.py
@@ -75,10 +75,13 @@ outTree(root)
 t = [1,2,3,5,6]
 
 all = []
+
 #target每组要输出的元素个数，step游标
 def combine(data, step, select_data, target_num):
     # 如果已经凑齐一组完成的组合，输出当前组
     if(len(select_data) == target_num):
+        print(tuple(select_data))
+        all.append(tuple(select_data))
         return select_data
     # 如果游标超过数组长，说明后续元素都被遍历完成，跳转到上一个元素的循环
     if(step >= len(data)):
@@ -86,10 +89,7 @@ def combine(data, step, select_data, target_num):
     # 把选中的元素加入临时输出列表中，n个一组作为输出
     select_data.append(data[step])
     # 递归调整最后一位
-    hh = combine(data, step + 1, select_data, target_num )
-    if(hh!=None):
-        all.append(hh)
-        print(hh)
+    combine(data, step + 1, select_data, target_num )
     # 构建一个新组合，首先要删掉上次输出组中药排除的元素
     select_data.pop()
     # 递归向上调整一位元素
@@ -97,7 +97,7 @@ def combine(data, step, select_data, target_num):
 
 combine(t, 0, [], q)
 print("all",all)
-
-a = [[1,2,3]]
-def aaa():
-    a.append([4])
+ls = []
+for a in all:
+    ls.append(list(a))
+print(ls,len(ls))
